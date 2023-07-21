b4ef08cda8c40edb5502d910f9fa6ad5d9f46d11
MoovendhanR <moovendhanr1011@gmail.com>
2023-07-21 12:43:28 +0530

    initial commit mantine-ui initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {  Text } from '@mantine/core';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Text>Mantine -ui testing </Text>
+    </div>
+  );
+}
+
+export default App;
